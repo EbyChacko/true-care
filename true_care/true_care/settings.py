78315d9b31b2dd2834c75257d1d16a13b848beb9
EbyChacko <cybe1990@gmail.com>
2024-01-30 12:11:08 +0000

    add base html and index html

--- true_care/true_care/settings.py
+++ true_care/true_care/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bn8ik4l@s_b5babch3z%y_$9#ku)h&+g&-l@w)07e5=4qw@^)v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'true_care.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
