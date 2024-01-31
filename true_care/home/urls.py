92598278eca43593490503b67461512bfe411a26
EbyChacko <cybe1990@gmail.com>
2024-01-31 00:52:17 +0000

    add basic pages:index,appoinment,contact,department,about etc

--- true_care/home/urls.py
+++ true_care/home/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('departments/', views.departments, name='departments'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('doctors/', views.doctors, name='doctors'),
 ]
--- true_care/home/views.py
+++ true_care/home/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def departments(request):
+    return render(request,'departments.html')
+
+def appointment(request):
+    return render(request,'appointment.html')
+
+def doctors(request):
+    return render(request,'appointment.html')        
