2e2b5668c76fa17255f6eca497555affabdeed24
EbyChacko <cybe1990@gmail.com>
2024-01-31 21:51:35 +0000

    feat:add department and patient models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=200, unique=True)
+    overview = models.TextField()
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering:['department_name']
+
+    def __str__(self):
+        return self.department_name
+
+
+class Patients(models.Model):
+    patient_name = models.CharField(max_length=250)
+    patient_age = models.TextField()
+    patient_address = models.TextField()
+    patient_zipcode = models.CharField(max_length=20)
+    patient_country = models.CharField(max_length=200)
+    patient_mobile = models.CharField(max_length=15)
+    patient_email = models.CharField(max_length=200)
+    patient_password = models.CharField(max_length=250)
+
+    class Meta:
+        ordering:['patient_name']
+
+    def __str__(self):
+        return self.patient_name
--- true_care/settings.py
+++ true_care/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
