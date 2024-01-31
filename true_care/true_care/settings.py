02ff674bd46e48261d05fcf4a9a645048e596876
EbyChacko <cybe1990@gmail.com>
2024-01-31 17:10:50 +0000

    fix the project name error in the procfile

--- true_care/true_care/settings.py
+++ true_care/true_care/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bn8ik4l@s_b5babch3z%y_$9#ku)h&+g&-l@w)07e5=4qw@^)v'
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
