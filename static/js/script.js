55ea753f17a2182a5ccd8df3ac3a9de7c504afbb
EbyChacko <cybe1990@gmail.com>
2024-02-24 11:24:56 +0000

    add validation for the login and signup forms

--- static/js/script.js
+++ static/js/script.js
@@ -57,98 +57,6 @@ function validateForm() {
 }
 
 
-// Update_details form validation
-function validateUpdateForm() {
-    console.log("inside the validator")
-    var name = document.getElementById("name").value;
-    var mobile = document.getElementById("mobile").value;
-    var email = document.getElementById("email").value;
-    var address = document.getElementById("address").value;
-    var dateOfBirth = document.getElementById("date_of_birth").value;
-    var country = document.getElementById("country").value;
-    var zipcode = document.getElementById("zipcode").value;
-
-    var nameError = document.getElementById("nameError");
-    var mobileError = document.getElementById("mobileError");
-    var emailError = document.getElementById("emailError");
-    var addressError = document.getElementById("addressError");
-    var dateOfBirthError = document.getElementById("dateOfBirthError");
-    var countryError = document.getElementById("countryError");
-    var zipcodeError = document.getElementById("zipcodeError");
-
-    // Reset previous error messages
-    nameError.innerHTML = "";
-    mobileError.innerHTML = "";
-    emailError.innerHTML = "";
-    addressError.innerHTML = "";
-    dateOfBirthError.innerHTML = "";
-    countryError.innerHTML = "";
-    zipcodeError.innerHTML = "";
-
-    var isValid = true;
-
-    // Validate Name
-    if (name.trim() === "") {
-        nameError.innerHTML = "Name is required.";
-        isValid = false;
-        nameError.style.visibility = "visible";
-    }
-
-    // Validate Mobile
-    function validateMobileNumber(mobile) {
-        var mobileValidations = /^(\+\d{1,3}\s?\d{1,}\s?\d{1,}\s?\d{1,}|\d{10})$/;
-        return mobileValidations.test(mobile);
-    }
-    if (mobile.trim() === "" || !validateMobileNumber(mobile)) {
-        mobileError.innerHTML = "Mobile is required and must be a 10 digit number.";
-        isValid = false;
-        mobileError.style.visibility = "visible";
-    }
-
-    // Validate Email
-    var emailExtentions = /^[^\s@]+@[^\s@]+\.(com|in|ie|org|net|edu|gov|mil|int|co|uk|us|au|)$/;
-    if (email.trim() === "" || !emailExtentions.test(email)) {
-        emailError.innerHTML = "Email is required and must be a valid email address.";
-        isValid = false;
-        emailError.style.visibility = "visible";
-    }
-
-    // Validate Address
-    if (address.trim() === "") {
-        addressError.innerHTML = "Address is required.";
-        isValid = false;
-        addressError.style.visibility = "visible";
-    }
-
-    // Validate Date of Birth
-    if (dateOfBirth.trim() === "") {
-        dateOfBirthError.innerHTML = "Date of Birth is required.";
-        isValid = false;
-        dateOfBirthError.style.visibility = "visible";
-    }
-
-    // Validate Country
-    if (country.trim() === "") {
-        countryError.innerHTML = "Country is required.";
-        isValid = false;
-        countryError.style.visibility = "visible";
-    }
-
-    // Validate Zip Code
-    if (zipcode.trim() === "") {
-        zipcodeError.innerHTML = "Zip Code is required.";
-        isValid = false;
-        zipcodeError.style.visibility = "visible";
-    }
-
-    return isValid;
-}
-
-
-function validateAppointmentForm(){
-    print("from the validate appointment function")
-}
-
 
 document.getElementById('photo-input').addEventListener('change', function(event) {
     var input = event.target;
@@ -190,4 +98,3 @@ $(document).ready(function() {
         }
     });
 });
-
